b22ac8ab57e7fcf5fbaf5fe3c0d4b4dc1f13113d
dougfoo <doug.cha@gmail.com>
2019-10-13 02:35:01 -0400

    added back build

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const snowRoutes = express.Router();
-const port = 9000;
+
+const PORT = process.env.PORT || 4000;
 
 var path = require('path');
 
@@ -25,7 +26,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./proj2", "build/web", "index.html"));
 });
 
-app.listen(port, function() {
-    console.log("Server is running on Port..: " + port);
+app.listen(PORT, function() {
+    console.log("Server is running on Port..: " + PORT);
 });
 
