d9f880c96f17129691b20d1a86e03d4fd266717d
dougfoo <doug.cha@gmail.com>
2019-10-14 23:45:56 -0400

    added click region test

--- proj2/lib/main.dart
+++ proj2/lib/main.dart
@@ -81,7 +81,8 @@ class MyApp extends StatelessWidget {
             titleSection,
             buttonSection,
             textSection,
-            chartSection
+            chartSection,
+            TapboxA()
           ],
         )
       ),
@@ -247,4 +248,43 @@ class SnowFall {
   final int snow;
 
   SnowFall(this.year, this.snow);
+}
+
+// testing out gestures
+
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
 }
